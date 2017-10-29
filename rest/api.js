e354122c09578fe1d8a5fe67160976921a06cfb7
Chris Hawkins <cthawki@pointpark.edu>
2017-10-29 10:27:08 -0400

    Create api.js

---
+++ rest/api.js
@@ -0,0 +1,13 @@
+// Dependencies
+var express = require('express');
+var router = express.Router();
+
+// Routes
+router.get('/courses', function(req, res){});
+  res.send('api is working');
+});
+
+router.get('/
+
+// Return
+module.exports = router;
