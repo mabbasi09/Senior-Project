96b1fdf6c4fc663d6764ef23ada4ba792f83ea53
Mohammad Al-Abbasi <sheikhhmo@gmail.com>
2017-11-25 20:32:04 -0500

    adding index.html, index.js, and ssaStyles.css

--- index.js
+++ index.js
@@ -6,56 +6,30 @@ const app = express();
 
 //Initialize MySQL and create connection
 const mysql = require('mysql');
+const credentials = require('./models/credentials.js');
+
 const db = mysql.createConnection({
-	host	: 'localhost',
-	user	: 'Mohammad',
-	password: '111111',
-	// database: 'ssa-db' 
+	host	: credentials.host,
+	user	: credentials.user,
+	password: credentials.password,
+	database: credentials.database
 });
 
 db.connect(function(err){
-	// if(err){
-	// 	throw err;
-	// }
-	console.log('MySQL connected...');
+	if(err){
+		console.log(err.stack);
+	} else {
+		console.log('MySQL connected...');
+	}
 });
 
-//body-parser utility
-const bodyParser = require('body-parser');
+var path = require('path');
+//app.use(express.static(path.join(__dirname, 'public')));
 
-//path utility
-const path = require('path');
 
-// var logger = function(req, res, next){
-// 	console.log('Logging...');
-// 	next();
-// }
+app.get('/', function(req, res){
 
-// app.use(logger);
-
-//View engine
-// app.set('view engine', 'ejs');
-// app.set('views', path.join(__dirname, 'views'));
-
-//Body Parser Middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-
-//Set Static Path
-app.use(express.static(path.join(__dirname, 'public')));
-
-// app.get('/createdb', function(req, res){
-
-// 	let sql = 'CREATE DATABASE ssa-db';
-
-// 	db.query(sql, function(err, result){
-// 		// if(err){
-// 		// 	throw err;
-// 		// }
-// 		console.log(result);
-// 		res.send('database created...')
-// 	});
-// });
+});
 
 app.listen(3000, function(){
 	console.log("server started on port 3000");
