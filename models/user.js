11b07bc18c6cd411eb0f1f01bce21a2c5e04c592
Mohammad Al-Abbasi <sheikhhmo@gmail.com>
2017-12-07 04:46:36 -0500

    adding changes to routes, frontend ajax request and course table

---
+++ models/user.js
@@ -0,0 +1,15 @@
+var bcrypt = require('bcryptjs');
+
+var User = {}
+
+module.exports = {
+	User: User,
+	createUser: function(newUser, callback){
+		bcrypt.genSalt(10, function(err, salt) {
+		    bcrypt.hash(newUser.password, salt, function(err, hash) {
+		        newUser.password = hash;
+		        // newUser.save(callback);
+		    });
+		});
+	}
+}
--- routes/main.js
+++ routes/main.js
@@ -1,31 +1,112 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../models/db');
+var user = require('../models/user');
+var db = require('../models/db').db;
+var dbConnect = require('../models/db').connection;
 
 //Homepage
 router.get('/', function(req, res){
 	res.render('main');
 });
 
-// Register
+// Register a user page
 router.get('/register', function(req, res){
 	res.render('register');
 });
 
-// Login
+// Register a user process
+router.post('/register', function(req, res){
+	var firstName = req.body.firstName;
+	var lastName = req.body.lastName;
+	var email = req.body.email;
+	var username = req.body.username;
+	var password = req.body.password;
+	var cpassword = req.body.cpassword;
+
+	//Validation
+	req.checkBody('firstName', 'First name is required!').notEmpty();
+	req.checkBody('lastName', 'Last name is required!').notEmpty();
+	req.checkBody('email', 'E-mail is invalid!').isEmail();
+	req.checkBody('username', 'Username is required!').notEmpty();
+	req.checkBody('password', 'Password is required!').notEmpty();
+	req.checkBody('cpassword', 'Passwords do not match!').equals(password);
+
+	var errors = req.validationErrors();
+
+	if(errors){
+		res.render('register', {
+			errors: errors
+		});
+		console.log(errors);
+	} else {
+		var newUser = user;
+
+		newUser.firstName = firstName;
+		newUser.lastName = lastName;
+		newUser.email = email;
+		newUser.username = username;
+
+		newUser.createUser(newUser, function(err, user){
+			if(err) throw err;
+		});
+		console.log(newUser);
+
+		req.flash('success_msg', 'You are now registered!');
+		res.redirect('login');
+	}
+});
+
+// Login page
 router.get('/login', function(req, res){
 	res.render('login');
 });
 
-//Catalog API
+// Login process
+router.post('/login', function(req, res){
+	// res.render('login');
+});
+
+//Course catalog data API
 router.get('/catalog', function(req, res){
-	
-	var sql = 'SELECT * FROM Course WHERE instructorLast = "Voortman";';
-	var query = db.db.query(sql, function(err, result, fields){
-		console.log(result);
-		res.json(result)
+
+	var sql = 'SELECT courseID, year, term, code, title FROM Course WHERE instructorLast = "Voortman";';
+	var query = db.query(sql, function(err, results, fields){
+		res.json(results);
 	});
-	// res.send('hello world');
+});
+
+//Add a course to schedule
+router.post('/updateSchedule', function(req, res){
+
+	//Tells us which courses to drop/add
+	var courseCodes = req.body.data;
+	var action = req.body.action;
+	console.log("User has requested to " + action + " course: " + courseCodes);
+
+	if (action == "add"){
+
+		for(var i = 0; i < courseCodes.length; i++){
+			// var sql = 'INSERT INTO Schedule title WHERE studentId = "56";';
+			// var query = db.query(sql, function(err, results, fields){
+			// 	console.log(results);
+			// });
+		}
+		res.send('done');
+
+	} else if (action == "drop"){
+		
+		for(var i = 0; i < courseCodes.length; i++){
+			// var sql = 'DELETE FROM Schedule title WHERE studentId = "56";';
+			// var query = db.query(sql, function(err, results, fields){
+			// 	console.log(results);
+			// });
+		}
+		res.send('done');
+
+	} else {
+		console.log("nothing to do!");
+		res.redirect('main');
+	}
 });
 
 module.exports = router;
