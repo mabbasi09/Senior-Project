fc989405d6cd66e53ead455b155152e4e48f307d
Chris Hawkins <cthawki@pointpark.edu>
2017-12-13 16:43:40 -0500

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -135,3 +135,21 @@ window.onload=function() {
     if (this.value !== 0) alert(choices[this.value]);
   }
 }    
+
+
+// Progress bar
+
+function move() {
+  var elem = document.getElementById("myBar");   
+  var width = 20;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+      elem.innerHTML = width * 1  + '%';
+    }
+  }
+}
